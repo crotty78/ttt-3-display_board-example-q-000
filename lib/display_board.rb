cdd7f884c4b358522c6d909fac481c8c8e4b509d
crotty78 <crotty147@gmail.com>
2016-03-14 22:43:03 +0000

    nitrous-backup at Mon Mar 14 22:43:03 UTC 2016

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,12 @@
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def=display_board
+  row1={" ","|"," ","|"," "}
+  puts row1
+  line1={----------}
+  puts line1
+  row2={" ","|"," ","|"," "}
+  puts row2
+  line2={----------}
+  puts line2
+  row3={" ","|"," ","|"," "}
+  puts row3
+end
